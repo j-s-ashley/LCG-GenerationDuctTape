b3b464ea1f2d48d3e9149812b7828e57d24da522
j-s-ashley <jashley6@vols.utk.edu>
2024-05-02 12:17:34 +0200

    Updates generation bash script test file to use automated process card

--- test_gen.sh
+++ test_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_ATLAS_6/x86_64-centos9-gcc11-opt/setup.sh
-mg5_aMC ranging_small_proc_card.dat > mg5.log
+mg5_aMC auto_proc_card.dat > mg5.log
 gunzip $PWD/PROC_RPVMSSM_UFO_0/Events/run_01/unweighted_events.lhe.gz
 ./main42 main42.cmnd out.hepmc > out.log
