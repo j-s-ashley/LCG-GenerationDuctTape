77bc2c58038c99afcb3539ec08d2410907fdfc73
j-s-ashley <jashley6@vols.utk.edu>
2024-05-02 13:53:33 +0200

    Refines naming scheme for MadGraph directories

--- make_proc_card.sh
+++ make_proc_card.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+card_name='auto_proc_card.dat'
+
 mass_start=$1
 mass_end=$2
 
-touch auto_proc_card.dat
-echo import model ../../DVMuReint/RPVMSSM_UFO/RPVMSSM_UFO/ > auto_proc_card.dat
-echo define q = g u c d s u~ c~ d~ s~ b t b~ t~ h01 h2 h3 h+ h- >> auto_proc_card.dat
-echo define p = g u c d s u~ c~ d~ s~ b b~ >> auto_proc_card.dat
-echo 'generate p p > mur- mur+ /q' >> auto_proc_card.dat
-echo output auto >> auto_proc_card.dat
-echo launch >> auto_proc_card.dat
-echo ./param_card_default.dat >> auto_proc_card.dat
-echo set nevents 20000 >> auto_proc_card.dat
-echo set time_of_flight 0.0 >> auto_proc_card.dat
-echo set small_width_treatment 1e-30 >> auto_proc_card.dat
-echo set mass 1000013 $mass >> auto_proc_card.dat
-echo set mass 2000013 $mass >> auto_proc_card.dat
-echo set decay 2000013 6.500000e-15 >> auto_proc_card.dat
+mass=$mass_start
+
+decay=1
+
+dir_name='smu'$mass'gev'$decay'ns'
+
+nevents=3
+
+touch $card_name
+echo import model ../../DVMuReint/RPVMSSM_UFO/RPVMSSM_UFO/ > $card_name
+echo define q = g u c d s u~ c~ d~ s~ b t b~ t~ h01 h2 h3 h+ h- >> $card_name
+echo define p = g u c d s u~ c~ d~ s~ b b~ >> $card_name
+echo 'generate p p > mur- mur+ /q' >> $card_name
+echo output auto >> $card_name
+echo launch -n $dir_name >> $card_name
+echo ./param_card_default.dat >> $card_name
+echo set nevents $nevents >> $card_name
+echo set time_of_flight 0.0 >> $card_name
+echo set small_width_treatment 1e-30 >> $card_name
+echo set mass 1000013 $mass >> $card_name
+echo set mass 2000013 $mass >> $card_name
+echo set decay 2000013 6.500000e-15 >> $card_name
 
 for ((mass=$mass_start+100;mass<=mass_end;mass=$mass+100)); do
-	echo launch >> auto_proc_card.dat
-	echo set mass 1000013 $mass >> auto_proc_card.dat
-	echo set mass 2000013 $mass >> auto_proc_card.dat
-	echo set decay 2000013 6.500000e-15 >> auto_proc_card.dat
+	dir_name='smu'$mass'gev'$decay'ns'
+	echo launch -n $dir_name >> $card_name
+	echo set mass 1000013 $mass >> $card_name
+	echo set mass 2000013 $mass >> $card_name
+	echo set decay 2000013 6.500000e-15 >> $card_name
 done
+
+echo done >> $card_name
--- test_gen.sh
+++ test_gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_ATLAS_6/x86_64-centos9-gcc11-opt/setup.sh
+bash make_proc_card.sh 300 500
 mg5_aMC auto_proc_card.dat > mg5.log
 gunzip $PWD/PROC_RPVMSSM_UFO_0/Events/run_01/unweighted_events.lhe.gz
 ./main42 main42.cmnd out.hepmc > out.log
