b0af86ceb28b91a9e9771ecb04f197cdc07a7f56
j-s-ashley <jashley6@vols.utk.edu>
2024-05-25 23:01:47 +0200

    Comments make_input.py

--- make_input.py
+++ make_input.py
@@ -1,8 +1,10 @@
 import numpy as np
 import shutil
 
+# Create input text file for HTCondor
 f = open("input.txt", "w")
 
+# --- Variable initialization --- #
 mass_start = float(input("Enter the minimum particle mass in GeV: "))
 mass_end = float(input("Enter the maximum particle mass in GeV: "))
 mass_increment = float(input("Enter the mass step size in GeV: "))
@@ -12,6 +14,7 @@ tau_start = float(input("Enter the minimum particle lifetime in ns: "))
 tau_end = float(input("Enter the maximum particle lifetime in ns: "))
 tau_increment = float(input("Enter the lifetime step size in ns: "))
 num_taus = int( ( (tau_end - tau_start) / tau_increment ) + 1 )
+# --- End variable initialization --- #
 
 print("Creating input card...")
 
@@ -20,6 +23,7 @@ for mass in np.linspace(mass_start, mass_end, num_masses):
         f.write( str(mass) + " " + str(tau) + "\n")
 
 f.close()
+# Input card needs to be included in submission directory
 shutil.move("input.txt","submission/input.txt")
 
 print("Input card 'input.txt' created and moved to the submission directory.")
